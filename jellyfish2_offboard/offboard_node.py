2b1c3712ab070a3586c26b362ba7b41f2cc69afd
MonkeScripts <shaolianghe0.0@gmail.com>
2024-06-27 20:09:42 +0800

    Simple sequence with no state machine

--- jellyfish2_offboard/offboard_node.py
+++ jellyfish2_offboard/offboard_node.py
@@ -1,11 +1,225 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-
-
+from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile, ReliabilityPolicy
+from px4_msgs.msg import VehicleStatus, OffboardControlMode
+from px4_msgs.msg import TakeoffStatus, VehicleCommand, VehicleCommandAck
+from px4_msgs.msg import TrajectorySetpoint, VehicleLocalPosition, LandingTargetPose
+import numpy as np
 class OffboardNode(Node): 
     def __init__(self):
         super().__init__("offboard_node")
-        self.get_logger().info("helo")
+        self.qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.SYSTEM_DEFAULT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1,
+        )
+        self.declare_parameter("hover_altitude", 10.0)
+        self.declare_parameter("vehicle_status_topic", "")
+        self.declare_parameter("offboard_heartbeat_topic", "")
+        self.declare_parameter("takeoff_status_topic", "")
+        self.declare_parameter("vehicle_command_topic", "")
+        self.declare_parameter("traj_setpoint_topic", "")
+        self.declare_parameter("local_pos_topic", "")
+        self.declare_parameter("landing_pos_topic", "")
+        self.hover_altitude = self.get_parameter(
+            "hover_altitude").get_parameter_value().double_value
+        self.vehicle_status_topic = self.get_parameter(
+            "vehicle_status_topic"
+        ).get_parameter_value().string_value
+        self.offboard_heartbeat_topic = self.get_parameter(
+            "offboard_heartbeat_topic"
+        ).get_parameter_value().string_value
+        self.takeoff_status_topic = self.get_parameter(
+            "takeoff_status_topic"
+        ).get_parameter_value().string_value
+        self.vehicle_command_topic = self.get_parameter(
+            "vehicle_command_topic"
+        ).get_parameter_value().string_value
+        self.traj_setpoint_topic = self.get_parameter(
+            "traj_setpoint_topic"
+        ).get_parameter_value().string_value
+        self.local_pos_topic = self.get_parameter(
+            "local_pos_topic"
+        ).get_parameter_value().string_value
+        self.landing_pos_topic = self.get_parameter(
+            "landing_pos_topic"
+        ).get_parameter_value().string_value
+
+        self.vehicle_status_sub_ = self.create_subscription(
+            VehicleStatus, self.vehicle_status_topic, 
+            self.vehicle_status_callback, self.qos_profile 
+        )
+        self.offboard_heartbeat_pub_ = self.create_publisher(
+            OffboardControlMode, 
+            self.offboard_heartbeat_topic, 
+            self.qos_profile
+        )
+        self.takeoff_status_sub_ = self.create_subscription(
+            TakeoffStatus,
+            self.takeoff_status_topic,
+            self.takeoff_status_callback,
+            self.qos_profile,
+        )
+        self.vehicle_command_pub_ = self.create_publisher(
+            VehicleCommand, 
+            self.vehicle_command_topic, 
+            self.qos_profile
+        )
+        self.traj_setpoint_pub_ = self.create_publisher(
+            TrajectorySetpoint, 
+            self.traj_setpoint_topic, 
+            self.qos_profile
+        )
+        self.local_pos_sub_ = self.create_subscription(
+            VehicleLocalPosition,
+            self.local_pos_topic,
+            self.local_pos_callback,
+            self.qos_profile,
+        )
+        self.landing_pos_pub_ = self.create_publisher(
+            LandingTargetPose, 
+            self.landing_pos_topic, 
+            self.qos_profile
+        )
+        self.offboard_setpoint_counter_ = 0
+        self.home_lat = 0.0
+        self.home_lon = 0.0
+        self.home_alt = 0.0
+        self.set_home_location()
+        self.timer_ = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        """
+        Continuously publish heartbeat and traj setpoint.
+        PX4 requires that the vehicle is already receiving 
+        OffboardControlMode messages before it will arm in offboard mode, 
+        or before it will switch to offboard mode when flying
+        """
+        if self.offboard_setpoint_counter_ < 100:
+            self.publish_traj_setpoint(0.0, 0.0,-10.0, 0.0)
+
+        if self.offboard_setpoint_counter_ == 50:
+            self.engage_offboard_mode()
+            self.arm()
+            self.publish_traj_setpoint(0.0, 0.0,-10.0, 0.0)
+
+        if (
+            self.offboard_setpoint_counter_ > 100
+            and self.offboard_setpoint_counter_ < 300
+        ):
+            self.publish_traj_setpoint(5.0, 5.0,-10.0, 0.0)
+
+        if self.offboard_setpoint_counter_ >= 300:
+            self.engage_land_mode()
+        self.publish_offboard_heartbeat()
+        self.offboard_setpoint_counter_ += 1
+
+    def vehicle_status_callback(self, msg:VehicleStatus):
+        self.get_logger().info(
+            f"Vehicle Status Timestamp: {msg.timestamp} Offboard status(14):{msg.nav_state}"
+        )
+
+    def takeoff_status_callback(self, msg:TakeoffStatus):
+        self.get_logger().info(
+            f"TAKEOFF Timestamp: {msg.timestamp} Status:{msg.takeoff_state}"
+        )
+
+    def local_pos_callback(self, msg:VehicleLocalPosition):
+        self.home_lat = msg.ref_lat
+        self.home_lon = msg.ref_lon
+        self.home_alt = msg._ref_alt
+        # self.get_logger().info(
+        #     f"Local pos Timestamp: {msg.timestamp} ref:{[msg.ref_lat, msg.ref_lon, msg.ref_alt]}"
+        # )
+
+    def set_home_location(self):
+        """
+        set home position to current location
+        Changes the home location either to the current location or a specified location. 
+        |Use current (1=use current location, 0=use specified location)| Empty| Empty| Empty| Latitude| Longitude| Altitude|
+        """
+        # Debug need to check whether home location works
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_DO_SET_HOME, param1=1.0)
+        self.get_logger().info("Set home location...")
+
+    # TODO Implement waypointing in the future
+    # TODO Keep as an arbitary value to test states
+    def publish_traj_setpoint(self, x:float, y:float, z:float, yaw:float):
+        """Publish pose relative to ned worldframe"""
+        msg = TrajectorySetpoint()
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        msg.position = [x, y, z]
+        msg.yaw = yaw
+        self.traj_setpoint_pub_.publish(msg)
+        self.get_logger().info(
+            f"Traj setpoint sent{[x, y, z], msg.yaw}....")
+
+    def publish_vehicle_command(self, command, **params) -> None:
+        """Publish a vehicle command."""
+        msg = VehicleCommand()
+        msg.command = command
+        msg.param1 = params.get("param1", 0.0)
+        msg.param2 = params.get("param2", 0.0)
+        msg.param3 = params.get("param3", 0.0)
+        msg.param4 = params.get("param4", 0.0)
+        msg.param5 = params.get("param5", 0.0)
+        msg.param6 = params.get("param6", 0.0)
+        msg.param7 = params.get("param7", 0.0)
+        msg.target_system = 1
+        msg.target_component = 1
+        msg.source_system = 1
+        msg.source_component = 1
+        msg.from_external = True
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.vehicle_command_pub_.publish(msg)
+
+    def arm(self):
+        """Arm drone. Param1=1.0 for arm."""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
+        self.get_logger().info("Arm command sent....")
+
+    def disarm(self):
+        """Disarm drone. Param1=0.0 for disarm."""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
+        self.get_logger().info("Disarm command sent....")
+
+    def publish_offboard_heartbeat(self):
+        """Publish offboard heartbeat."""
+        msg = OffboardControlMode()
+        msg.position = True
+        msg.velocity = False
+        msg.acceleration = False
+        msg.attitude = False
+        msg.body_rate = False
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.offboard_heartbeat_pub_.publish(msg)
+        self.get_logger().info("Publishing offboard heartbeat....")
+
+    def engage_offboard_mode(self):
+        """Switch mode to offboard mode"""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
+        self.get_logger().info("Switching to offboard mode....")
+
+    def engage_land_mode(self):
+        """
+        Switch mode to landmode with opportunistic landing.
+        Precision landing can be initiated as part of a mission using 
+        MAV_CMD_NAV_LAND with param2 set appropriately:
+
+        0: Normal landing without using the target.
+        1: Opportunistic precision landing.
+        2: Required precision landing.
+        """
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_NAV_LAND, param2=1.0
+        )
+        self.get_logger().info("Sent Land command....")
 
 
 def main(args=None):
@@ -13,5 +227,6 @@ def main(args=None):
     node = OffboardNode()
     rclpy.spin(node)
     rclpy.shutdown()
+
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 import os 
-import glob
+from glob import glob
 package_name = 'jellyfish2_offboard'
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="1.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -20,6 +23,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["offboard_test = jellyfish2_offboard.offboard_node:main"],
+        "console_scripts": ["offboard_node = jellyfish2_offboard.offboard_node:main"],
     },
 )
